a80c1ca071d7c7f60a7511793a35a317ff6cd7f6
marvin <marvzknows03@gmail.com>
2026-02-12 16:46:19 +0800

    employer initial api route and controller

---
+++ app/Http/Controllers/EmployerController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmployerController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        return ['message' => 'CREATE employer profile'];
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return ['message' => 'VIEW employer profile'];
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        return ['message' => 'UPDATE employer profile'];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        return ['message' => 'REMOVE employer profile'];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\EmployerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,16 +14,24 @@ Route::get('/test', function () {
 });
 
 // Authentication routes
-Route::post('/register', [App\Http\Controllers\Auth\AuthController::class, 'register']);
-Route::post('/login', [App\Http\Controllers\Auth\AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware([
     'api',
     'auth:sanctum',
 ])->group(function () {
-    Route::post('/logout', [App\Http\Controllers\Auth\AuthController::class, 'logout'])
+    Route::post('/logout', [AuthController::class, 'logout'])
         ->name('logout');
 
-    Route::get('/me', [App\Http\Controllers\Auth\AuthController::class, 'me'])
+    Route::get('/me', [AuthController::class, 'me'])
         ->name('me');
+});
+
+// Employer routes
+Route::middleware(['auth:sanctum'])->prefix('employer')->group(function () {
+    Route::post('/create', [EmployerController::class, 'store'])->name('employer.store');
+    Route::get('/{id}', [EmployerController::class, 'show'])->name('employer.show');
+    Route::put('/{id}', [EmployerController::class, 'update'])->name('employer.update');
+    Route::delete('/{id}', [EmployerController::class, 'destroy'])->name('employer.destroy');
 });
